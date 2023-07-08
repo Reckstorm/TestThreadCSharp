4c8bf5bc46793b6f5d80b173644bc781c3c66ac2
Mihail Sevagin <lolvillian@gmail.com>
2023-07-08 13:09:43 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿//new Thread(() =>
+//{
+//    while (true)
+//    {
+//        File.WriteAllText(@"C:\Users\Admin\Desktop\1.txt", $"{DateTime.Now.ToLocalTime()}\n");
+//        Console.WriteLine($"{DateTime.Now.ToLocalTime()}");
+//        Thread.Sleep(60000);
+//    }
+//}).Start();
+//Console.Read();
+
+using System.Runtime.CompilerServices;
+
+List<string> list = new List<string>();
+int sum = 0;
+
+Thread readTh = new Thread(() =>
+{
+    list.AddRange(File.ReadAllLines(@"C:\Users\Admin\Desktop\1.txt"));
+});
+
+
+Thread sumTh = new Thread(() =>
+{
+    list.ForEach(x => sum += int.Parse(x));
+    Console.WriteLine(sum);
+});
+
+
+readTh.Start();
+readTh.Join();
+sumTh.Start();
+
